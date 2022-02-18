90c85cc4d579605f746421dc73575466be8f2d48
Kyle Campbell <kc@kc.io>
2022-02-17 21:11:23 -0800

    replicate tests

--- main.ts
+++ main.ts
@@ -1,15 +1,48 @@
 import { Construct } from "constructs";
-import { App, TerraformStack, RemoteBackend, TerraformOutput } from "cdktf";
+import { App, TerraformStack, RemoteBackend, TerraformOutput, TerraformAsset } from "cdktf";
+import * as NullProvider from "./.gen/providers/null";
+import * as local from "./.gen/providers/local";
+import { RandomProvider, Password } from "./.gen/providers/random";
+import * as path from "path";
 
 class SourceStack extends TerraformStack {
   public password: any;
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    this.password = 'THIS IS THE VALUE'
+    new NullProvider.NullProvider(this, "null", {});
+    new RandomProvider(this, "random", {});
+    new local.LocalProvider(this, "local", {});
+
+    this.password = new Password(this, "password", {
+      length: 32,
+    });
+
+    new local.File(this, "file", {
+      filename: "../../../origin-file.txt",
+      content: this.password.result,
+    });
+
+    const nullResouce = new NullProvider.Resource(this, "test", {});
+
+    nullResouce.addOverride("provisioner", [
+      {
+        "local-exec": {
+          command: `echo "hello deploy"`,
+        },
+      },
+    ]);
 
     new TerraformOutput(this, "output", {
       value: "constant value",
+    });
+
+    const asset = new TerraformAsset(this, "asset-a", {
+      path: path.resolve(__dirname, "fixtures/a.txt"),
+    });
+
+    new TerraformOutput(this, "isAssetPresent", {
+      value: `\${fileexists("\${path.module}/${asset.path}") ? "yes" : "no"}`,
     });
 
     new RemoteBackend(this, {
@@ -23,8 +56,15 @@ class SourceStack extends TerraformStack {
 }
 
 class ConsumerStack extends TerraformStack {
-  constructor(scope: Construct, name: string, password: any) {
+  constructor(scope: Construct, name: string, password: Password) {
     super(scope, name);
+
+    new local.LocalProvider(this, "local", {});
+
+    new local.File(this, "file", {
+      filename: "../../../consumer-file.txt",
+      content: password.result,
+    });
 
     new TerraformOutput(this, "password", {
       value: password
